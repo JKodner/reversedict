a90f949bf2193dc0ad43318114b1be392c230f8b
Jacob Kodner <jake@kodner.net>
2014-02-22 09:24:28 -0800

    Put duplicate keys in list

--- reversedict.py
+++ reversedict.py
@@ -1,15 +1,22 @@
 def reversedict(d):
 	"""Reverses a Dictionary so that the values are the keys and the keys are the values"""
-	unhashables = [{}]
+	unhashables = {}
+	non_duplicates = {}
+	duplicates = {}
 	reverse = zip(d.values(), d.keys())
 	for i in reverse:
 		val = i[0]
 		try: 
 			hash(val)
 		except TypeError:
-			unhashables[0].update({i[1]: i[0]})
+			unhashables.update({i[1]: i[0]})
 			reverse.remove(i)
-	newdict = dict(reverse)
-	newdict.update({"unhashable_types": unhashables})
-	return newdict
+			continue
+		if val not in non_duplicates.keys():
+			non_duplicates.update({i[0]: i[1]})
+		else:
+			duplicates.update({i[1]: i[0]})
+	non_duplicates.update({"unhashable_types": unhashables})
+	non_duplicates.update({"returned_duplicates": duplicates})
+	return non_duplicates
 
