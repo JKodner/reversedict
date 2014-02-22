98b4e9007be003b00a41d80a82a9a0cacbc10e23
Jacob Kodner <jake@kodner.net>
2014-02-22 08:11:41 -0800

    Put unhashable types in list

--- reversedict.py
+++ reversedict.py
@@ -1,5 +1,15 @@
 def reversedict(d):
 	"""Reverses a Dictionary so that the values are the keys and the keys are the values"""
+	unhashables = [{}]
 	reverse = zip(d.values(), d.keys())
-	new_dict = dict(reverse)
-	return new_dict
+	for i in reverse:
+		val = i[0]
+		try: 
+			hash(val)
+		except TypeError:
+			unhashables[0].update({i[1]: i[0]})
+			reverse.remove(i)
+	newdict = dict(reverse)
+	newdict.update({"unhashable_types": unhashables})
+	return newdict
+
